86ced32c97f16e706c9a3fccf98fef5e13420e23
pravallika <tannirupravallika74@gmail.com>
2024-08-29 19:15:15 +0530

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.css'
+
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>Copyright @2024, Cryptoplace - All Right Reserved</p>
+    </div>
+  )
+}
+
+export default Footer
